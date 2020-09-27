9c9ef71d87d5c265d6b5c2fd348adcacc7d655f6
Simon Castagna <sd.castagna@gmail.com>
2020-09-27 19:31:54 +0200

    Updates

---
+++ precache-manifest.37d53fbfbf692843584f89a7fe14964a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a570149e854c6caa6efcae6ee7c200",
+    "url": "/bjcp-styleguide/index.html"
+  },
+  {
+    "revision": "fff4679caa4ccef30eda",
+    "url": "/bjcp-styleguide/static/css/main.cedf53aa.chunk.css"
+  },
+  {
+    "revision": "65078d7acf451bc5d11a",
+    "url": "/bjcp-styleguide/static/js/2.b7c09fa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bjcp-styleguide/static/js/2.b7c09fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff4679caa4ccef30eda",
+    "url": "/bjcp-styleguide/static/js/main.4a765a1d.chunk.js"
+  },
+  {
+    "revision": "f90d883930fef17fd2b5",
+    "url": "/bjcp-styleguide/static/js/runtime-main.8ce3242b.js"
+  }
+]);
